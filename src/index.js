a11e91b3ded052fdd8b1e98d0a6d6fd2e53268e2
Ipshita <Raillsreact@gmail.com>
2023-02-10 02:48:41 +0500

    removed extra line

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Index from './Components/App';
 import App from "./Components/App";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Index />
+    <App/>
   </React.StrictMode>
 );
 
